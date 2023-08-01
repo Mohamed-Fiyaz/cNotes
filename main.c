0a8fb249e64497b80f335b668cc7593081eafb08
Mohamed-Fiyaz <mohamedfiyaz@gmail.com>
2023-08-01 10:40:06 +0530

    New notes.

--- main.c
+++ main.c
@@ -35,9 +35,6 @@ int main(){
 
     return 0;
 }
-
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
@@ -60,3 +57,117 @@ int main(){
     free(ptr);
     return 0;
 }
+
+// Matrix Addition
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+    int a[r][c],b[r][c],add[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("Enter the elements for a[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("Enter the elements for b[%d][%d]: ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            add[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",add[i][j]);
+        }printf("\n");
+    }
+    return 0;
+
+}
+
+// Matrix Transpose
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+    int a[r][c],t[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("Enter the elements for a[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(int i=0;i<r;i++)
+        for(int j=0; j<c;j++)
+            t[i][j]=a[j][i];
+
+    printf("Original Matrix: \n");
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",a[i][j]);
+        }printf("\n");
+    }
+
+    printf("Transpose: \n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",t[i][j]);
+        }printf("\n");
+    }
+    return 0;
+}
+
+// Matrix Multiplication
+#include<stdio.h>
+int main() {
+    int r, c;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+    int a[r][c], b[r][c], mul[r][c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("Enter the elements for a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("Enter the elements for b[%d][%d]: ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            mul[i][j]=0;
+            for (int k = 0; k < c; k++) {
+                mul[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",mul[i][j]);
+        }printf("\n");
+    }
+    return 0;
+}
+*/
